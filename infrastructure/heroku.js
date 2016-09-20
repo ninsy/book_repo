026cbcf6dd91a956cd09b94fa7a9207b8245d85d
wojcieck <kamil.wojciechowsk@contractors.roche.com>
2016-09-20 08:14:58 +0200

    Added infrastructure settings

---
+++ infrastructure/heroku.js
@@ -0,0 +1,35 @@
+var heroin = require('heroin-js');
+
+var configurator = heroin(process.env.HEROKU_API_TOKEN);
+
+configurator.export("servicebookinventory").then(function(result) {
+    console.log(result);
+});
+
+var prod = { name: 'servicebookinventory',
+    organization: undefined,
+    region: 'eu',
+    maintenance: false,
+    stack: 'cedar-14',
+    config_vars: {
+        MONGODB_URI: process.env.MONGODB_URI
+    },
+    addons: {},
+    collaborators: [
+        'paaweel01@gmail.com',
+        'kamil.wojciechowski@contractors.roche.com'
+    ],
+    features: {
+        'runtime-dyno-metadata': { enabled: false },
+        'log-runtime-metrics': { enabled: false },
+        'http-session-affinity': { enabled: false },
+        preboot: { enabled: false },
+        'http-shard-header': { enabled: false },
+        'http-end-to-end-continue': { enabled: false },
+        'http-sni': { enabled: false },
+        'app-alerting': { enabled: false }
+    },
+    formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+    log_drains: [],
+    domains: [ 'servicebookinventory.herokuapp.com' ]
+};
